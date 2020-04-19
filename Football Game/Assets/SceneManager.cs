4e394739c8973245216048482d73953517def3c8
SMiLEY1337 <38574408+SMiLEY1337@users.noreply.github.com>
2020-04-20 01:07:53 +1000

    Update 2

---
+++ Football Game/Assets/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static int sceneIndex = 1;
+
+    public void changeScene(int index)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+    }
+}
